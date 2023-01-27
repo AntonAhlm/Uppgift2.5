2eb3dc109acdbdf1f33963c0123310bb7ccb9afa
Anton <>
2023-01-27 09:07:12 +0100

    Add project files.

---
+++ Uppgift2.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv en beräkning i formatet tal1*tal2 : " );
+            string Beräkning = Console.ReadLine();
+
+            int GångerIndex = Beräkning.IndexOf("*");
+
+            string strTal1 = Beräkning[..GångerIndex];
+
+            string strTal2 = Beräkning[(GångerIndex + 1)..];
+
+            double Tal1 = Convert.ToDouble(strTal1);
+            double Tal2 = Convert.ToDouble(strTal2);
+
+            Console.WriteLine(Tal1 + " * " + Tal2 + " = " + (Tal1*Tal2));
+
+        }
+    }
+}
